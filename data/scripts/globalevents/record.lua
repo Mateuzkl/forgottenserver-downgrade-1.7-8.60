55fba5b9b449e657005045de533c64365760aab6
Mateuzkl <mateus.roberto@educacao.mg.gov.br>
2026-02-04 12:59:28 -0300

    Revert "Move XML to GlobalEvent Revscripts"
    This reverts commit 067029ed4bb89013d3dffc4e4faa037b1f30807f.

--- data/scripts/globalevents/record.lua
+++
@@ -1,7 +0,0 @@
-local record = GlobalEvent("PlayerRecord")
-function record.onRecord(current, old)
-    addEvent(Game.broadcastMessage, 150, "New record: " .. current .. " players are logged in.", MESSAGE_STATUS_DEFAULT)
-    return true
-end
-record:type("record")
-record:register()
