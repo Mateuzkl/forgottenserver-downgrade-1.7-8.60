3a39a65a2591fb5cb1e0f8f513a46c0a96104ef1
Skyyzyy <mcl_d2@hotmail.com>
2026-02-26 14:27:22 -0300

    Revert "fix: adjust protocol and player structure issues"
    This reverts commit d1112fa759bea73faf4400a8272c23b72e64bf92.

--- src/game.h
+++ src/game.h
@@ -110,20 +110,18 @@ public:
 	static std::string getTradeErrorDescription(ReturnValue ret, Item* item);
 
 	// Struct to store damage info for a rewardboss
-	struct PlayerScoreInfo
-	{
-		int32_t damageDone = 0;  // Damage dealt by the player to the boss
-		int32_t damageTaken = 0; // Damage taken by the player from the boss
-		int32_t healingDone = 0; // Healing done to players near boss
+	struct PlayerScoreInfo {
+		int32_t damageDone = 0;   // Damage dealt by the player to the boss
+		int32_t damageTaken = 0;  // Damage taken by the player from the boss
+		int32_t healingDone = 0;  // Healing done to players near boss
 	};
 	// Struct to store contribution info for a rewardboss
-	struct RewardBossContributionInfo
-	{
-		std::unordered_map<uint32_t, PlayerScoreInfo> playerScoreTable; // Map of player GUIDs to PlayerScoreInfo
+	struct RewardBossContributionInfo {
+		std::unordered_map<uint32_t, PlayerScoreInfo> playerScoreTable;  // Map of player GUIDs to PlayerScoreInfo
 	};
 	// Map to track the contributions of players to different rewardbosses
 	std::unordered_map<uint32_t, RewardBossContributionInfo> rewardBossTracking;
-	void resetDamageTracking(uint32_t monsterId); // Function to reset damage tracking for a specific monster
+	void resetDamageTracking(uint32_t monsterId);  // Function to reset damage tracking for a specific monster
 
 	/**
 	 * Returns a creature based on the unique creature identifier
@@ -477,8 +475,8 @@ public:
 	void incrementMotdNum() { motdNum++; }
 
 	const std::unordered_map<uint32_t, Player*>& getPlayers() const { return players; }
-	const std::unordered_map<uint32_t, Npc*>& getNpcs() const { return npcs; }
-	const std::unordered_map<uint32_t, Monster*>& getMonsters() const { return monsters; }
+	const std::map<uint32_t, Npc*>& getNpcs() const { return npcs; }
+	const std::map<uint32_t, Monster*>& getMonsters() const { return monsters; }
 
 	void addPlayer(Player* player);
 	void removePlayer(Player* player);
@@ -507,10 +505,11 @@ public:
 
 	std::vector<Player*> getLiveCasters(const std::string& password);
 
-	void addLiveCaster(Player* player) { liveCasters.push_back(player); }
+	void addLiveCaster(Player* player) {
+		liveCasters.push_back(player);
+	}
 
-	void removeLiveCaster(Player* player)
-	{
+	void removeLiveCaster(Player* player) {
 		liveCasters.erase(std::remove(liveCasters.begin(), liveCasters.end(), player), liveCasters.end());
 	}
 
@@ -564,8 +563,8 @@ private:
 
 	WildcardTreeNode wildcardTree{false};
 
-	std::unordered_map<uint32_t, Npc*> npcs;
-	std::unordered_map<uint32_t, Monster*> monsters;
+	std::map<uint32_t, Npc*> npcs;
+	std::map<uint32_t, Monster*> monsters;
 
 	// list of items that are in trading state, mapped to the player
 	std::map<Item*, uint32_t> tradeItems;
@@ -574,8 +573,7 @@ private:
 
 	std::unordered_set<Tile*> tilesToClean;
 
-	ModalWindow offlineTrainingWindow{std::numeric_limits<uint32_t>::max(), "Train while you sleep",
-	                                  "Please choose a skill to train:"};
+	ModalWindow offlineTrainingWindow { std::numeric_limits<uint32_t>::max(), "Train while you sleep", "Please choose a skill to train:" };
 
 	static constexpr uint8_t LIGHT_DAY = 250;
 	static constexpr uint8_t LIGHT_NIGHT = 40;
