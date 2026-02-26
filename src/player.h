b43e92c93f32bd9e025e3e53969fd6c28de7104c
Skyyzyy <mcl_d2@hotmail.com>
2026-02-26 14:24:27 -0300

    Revert "feat: refactor formatting and syntax"
    This reverts commit 8cca92d6be0e187990a47176dc46e1493bf01c32.

--- src/player.h
+++ src/player.h
@@ -654,13 +654,15 @@ public:
 	                      int32_t oldStackPos, bool teleport)
 	{
 		if (client) {
-			client->sendMoveCreature(creature, newPos, newStackPos, oldPos, oldStackPos, teleport);
-			if (isLiveCasting()) {
+			if (creature == this && isLiveCasting()) {
+				client->sendMoveCreature(creature, newPos, newStackPos, oldPos, oldStackPos, teleport);
 				for (auto& spectator : spectators) {
 					if (spectator && spectator->isAcceptingPackets()) {
 						spectator->sendMoveCreature(creature, newPos, newStackPos, oldPos, oldStackPos, teleport);
 					}
 				}
+			} else {
+				client->sendMoveCreature(creature, newPos, newStackPos, oldPos, oldStackPos, teleport);
 			}
 		}
 	}
