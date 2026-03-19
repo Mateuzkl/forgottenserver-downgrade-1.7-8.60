76fff6ca8aa7e47a61b54a46710eb71c806716b2
G.Joker👻 <125809602+Mateuzkl@users.noreply.github.com>
2026-03-19 07:56:57 -0300

    fix ids

--- data/scripts/magic-roulette-master/configroulettevertical.lua
+++ data/scripts/magic-roulette-master/configroulettevertical.lua
@@ -8,25 +8,25 @@ local Slot = require('data/scripts/magic-roulette-master/lib/classes/slot')
 return {
 	slots = {
 		[17324] = Slot {
-			needItem = {id = 2972, count = 1},
+			needItem = {id = 2160, count = 1},
 			tilesPerSlot = 7,
 			orientation = "vertical",
 			centerPosition = Position(97, 117, 6),
 
 			items = {
-				{id = 3366, count = 1, chance = 0.2, rare = true},  -- Magic Plate Armor
-				{id = 3365, count = 1, chance = 0.3, rare = true},  -- Golden Helmet
-				{id = 3278, count = 1, chance = 0.5, rare = true},  -- Magic Longsword
-				{id = 3420, count = 1, chance = 9},   -- Demon Shield
-				{id = 3369, count = 1, chance = 9},   -- Warrior Helmet
-				{id = 3381, count = 1, chance = 9},   -- Crown Armor
-				{id = 3371, count = 1, chance = 9},   -- Knight Legs
-				{id = 3554, count = 1, chance = 9},   -- Steel Boots
-				{id = 3079, count = 1, chance = 9},   -- Boots of Haste
-				{id = 3051, count = 1, chance = 9},   -- Energy Ring
-				{id = 3055, count = 1, chance = 9},   -- Platinum Amulet
-				{id = 3057, count = 1, chance = 9},   -- Amulet of Loss
-				{id = 3155, count = 1, chance = 9},   -- Sudden Death Rune
+				{id = 2472, count = 1, chance = 0.2, rare = true},  -- Magic Plate Armor
+				{id = 2471, count = 1, chance = 0.3, rare = true},  -- Golden Helmet
+				{id = 2390, count = 1, chance = 0.5, rare = true},  -- Magic Longsword
+				{id = 2520, count = 1, chance = 9},   -- Demon Shield
+				{id = 2475, count = 1, chance = 9},   -- Warrior Helmet
+				{id = 2487, count = 1, chance = 9},   -- Crown Armor
+				{id = 2477, count = 1, chance = 9},   -- Knight Legs
+				{id = 2645, count = 1, chance = 9},   -- Steel Boots
+				{id = 2195, count = 1, chance = 9},   -- Boots of Haste
+				{id = 2167, count = 1, chance = 9},   -- Energy Ring
+				{id = 2171, count = 1, chance = 9},   -- Platinum Amulet
+				{id = 2173, count = 1, chance = 9},   -- Amulet of Loss
+				{id = 2268, count = 1, chance = 9},   -- Sudden Death Rune
 				{id = 5801, count = 1, chance = 9}    -- Jewelled Backpack
 			},
 		},
